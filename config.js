146910b6234a23f714b01829b038f2484131b5be
Jorge Solis <jso@escaux.com>
2021-03-27 17:01:33 +0100

    config update ignore

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mqtt: 'http://localhost',
-  url: 'http://localhost:8080/api',
-  topic: 'miflora/BonsaiCarmona',
-  auth: 'Basic ' + Buffer.from('user:password').toString('base64'),
-  picture: './picture.jpg'
-}
